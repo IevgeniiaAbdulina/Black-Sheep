d8c5a27dee87440a9d623596f9d2d1077f66e5cd
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2026-03-25 23:04:42 +0100

    feat: add about-gestalt section.

---
+++ src/app/features/landing/components/about-gestalt/about-gestalt.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-about-gestalt',
+  imports: [],
+  templateUrl: './about-gestalt.html',
+  styleUrl: './about-gestalt.css',
+})
+export class AboutGestalt {
+}
--- src/app/features/landing/pages/landing-page/landing-page.ts
+++ src/app/features/landing/pages/landing-page/landing-page.ts
@@ -1,10 +1,12 @@
 import {Component} from '@angular/core';
 import {Hero} from '../../components/hero/hero';
+import {AboutGestalt} from '../../components/about-gestalt/about-gestalt';
 
 @Component({
   selector: 'app-landing-page',
   imports: [
     Hero,
+    AboutGestalt
   ],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css',
