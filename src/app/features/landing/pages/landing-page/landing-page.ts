e751643b603b88a693fb767539156d75b16da58e
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2026-03-25 23:14:43 +0100

    feat: add about-me section.

--- src/app/features/landing/pages/landing-page/landing-page.ts
+++ src/app/features/landing/pages/landing-page/landing-page.ts
@@ -1,12 +1,14 @@
 import {Component} from '@angular/core';
 import {Hero} from '../../components/hero/hero';
 import {AboutGestalt} from '../../components/about-gestalt/about-gestalt';
+import {AboutMe} from '../../components/about-me/about-me';
 
 @Component({
   selector: 'app-landing-page',
   imports: [
     Hero,
-    AboutGestalt
+    AboutGestalt,
+    AboutMe
   ],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css',
