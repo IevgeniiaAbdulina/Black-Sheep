fda158625606a422cbe235ade55b672696476c57
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2026-03-25 22:52:16 +0100

    feat: add hero section.

--- src/app/features/landing/pages/landing-page/landing-page.ts
+++ src/app/features/landing/pages/landing-page/landing-page.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Hero} from '../../components/hero/hero';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [
+    Hero,
+  ],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css',
 })
-export class LandingPage {}
+export class LandingPage {
+}
