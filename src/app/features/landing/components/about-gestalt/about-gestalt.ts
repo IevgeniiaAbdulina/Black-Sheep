610c436bb0989cb1f154de8474c05b7bc5fc53a3
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2026-04-04 17:35:26 +0200

    feat: add basic style for About-Gestalt section.

--- src/app/features/landing/components/about-gestalt/about-gestalt.ts
+++ src/app/features/landing/components/about-gestalt/about-gestalt.ts
@@ -1,12 +1,19 @@
 import {Component} from '@angular/core';
 import * as data from '../../data-access/gestalt-data.json';
+import {Button} from '../../../../shared/components/button/button';
 
 @Component({
   selector: 'app-about-gestalt',
-  imports: [],
+  imports: [
+    Button
+  ],
   templateUrl: './about-gestalt.html',
   styleUrl: './about-gestalt.css',
 })
 export class AboutGestalt {
   protected readonly gestalt = data;
+
+  getContact(): void {
+    console.log("Contact me");
+  }
 }
