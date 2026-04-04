00afe29ffacfb8c8e8d7c78e408585c55c22ac57
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2026-04-04 17:15:48 +0200

    feat: basic style for About-me section.

--- src/app/features/landing/components/about-me/about-me.ts
+++ src/app/features/landing/components/about-me/about-me.ts
@@ -1,12 +1,21 @@
 import {Component} from '@angular/core';
 import * as data from '../../data-access/about-me-data.json';
+import {Button} from '../../../../shared/components/button/button';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-about-me',
-  imports: [],
+  imports: [
+    Button,
+    NgClass
+  ],
   templateUrl: './about-me.html',
   styleUrl: './about-me.css',
 })
 export class AboutMe {
   protected readonly aboutMe = data;
+
+  getContact(): void {
+    console.log("Contact me");
+  }
 }
