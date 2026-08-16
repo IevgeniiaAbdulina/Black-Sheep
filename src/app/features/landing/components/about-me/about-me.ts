f686fb32d6450c0f94945e6b4bc38ca2285e93d9
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2026-08-16 21:10:23 +0200

    feat: add illustrations.

--- src/app/features/landing/components/about-me/about-me.ts
+++ src/app/features/landing/components/about-me/about-me.ts
@@ -1,13 +1,14 @@
 import {Component, signal} from '@angular/core';
 import * as data from '../../data-access/about-me-data.json';
 import {Button} from '../../../../shared/components/button/button';
-import {NgClass} from '@angular/common';
+import {NgClass, NgOptimizedImage} from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
 
 interface CardData {
   id: number;
   name: string;
-  description: string;
+  description?: string;
+  src?: string;
 }
 
 @Component({
@@ -15,7 +16,8 @@ interface CardData {
   imports: [
     Button,
     NgClass,
-    MatCardModule
+    MatCardModule,
+    NgOptimizedImage
   ],
   templateUrl: './about-me.html',
   styleUrl: './about-me.css',
@@ -24,12 +26,12 @@ export class AboutMe {
   protected readonly aboutMe = data;
 
   cards = signal<Array<CardData>>([
-    {id: 1, name: 'Card One', description: ''},
-    {id: 2, name: 'Card Two', description: this.aboutMe.withWhom.content[0]},
-    {id: 3, name: 'Card Three', description: ''},
-    {id: 4, name: 'Card Four', description: this.aboutMe.withWhom.content[1]},
-    {id: 5, name: 'Card Five', description: ''},
-    {id: 6, name: 'Card Six', description: this.aboutMe.withWhom.content[2]}
+    {id: 1, name: 'Painting girl', src: './assets/images/paint-girl-black.png'},
+    {id: 2, name: 'Work with', description: this.aboutMe.withWhom.content[0]},
+    {id: 3, name: 'Painting boy', src: './assets/images/paint-boy-black.png'},
+    {id: 4, name: 'Work with', description: this.aboutMe.withWhom.content[1]},
+    {id: 5, name: 'Two girls', src: './assets/images/two-girls-black.png'},
+    {id: 6, name: 'Work with', description: this.aboutMe.withWhom.content[2]}
   ]);
 
   getContact(): void {
