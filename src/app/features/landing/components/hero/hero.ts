2b98e9d7fef1d4ea28926cfb1e8e6050998ccae6
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2026-08-17 21:55:32 +0200

    fix: add hero buttons navigation.

--- src/app/features/landing/components/hero/hero.ts
+++ src/app/features/landing/components/hero/hero.ts
@@ -1,7 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
 import * as data from '../../data-access/hero-data.json';
 import {Button} from '../../../../shared/components/button/button';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-hero',
@@ -10,5 +11,14 @@ import {Button} from '../../../../shared/components/button/button';
   styleUrl: './hero.css',
 })
 export class Hero {
+  protected router = inject(Router);
   protected readonly hero = data;
+
+  goToContact(): void {
+    this.router.navigate(['/'], {fragment: 'contact'});
+  }
+
+  goToTherapy(): void {
+    this.router.navigate(['/'], {fragment: 'about-gestalt'});
+  }
 }
